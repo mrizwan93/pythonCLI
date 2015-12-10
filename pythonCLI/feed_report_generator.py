0b2a22791cbb08dc5956d36e8fb2576d47c523c8
rizwanshaikh1055 <mail2rizwanbsl@gmail.com>
2015-12-11 00:42:03 +0530

    -- modified feed_report_generator file

--- pythonCLI/feed_report_generator.py
+++ pythonCLI/feed_report_generator.py
@@ -1,35 +1,77 @@
 import feedparser
-d = feedparser.parse('http://fedoraplanet.org/atom.xml')
-print d
 
-entries=open("entries.txt","wb")		#file to store all entries
-report = open("report.txt", "wb")		#file to store the report
-for i in d.entries:
-	i=dict(i)
-	entries.write(str(i))
+sources = ['http://planet.gnome.org/atom.xml',
+			'https://planetkde.org/rss20.xml',
+			'http://planet.openstack.org/rss20.xml',
+			'http://planetpython.org/rss20.xml',
+			'http://fedoraplanet.org/atom.xml']
+files = {"http://planet.gnome.org/atom.xml":"planetGnomeReport.txt",
+		 "https://planetkde.org/rss20.xml":"planetKdeReport.txt",
+		 "http://planet.openstack.org/rss20.xml":"planetOpenStackReport.txt",
+		 "http://planetpython.org/rss20.xml":"planetPythonReport.txt",
+		 "http://fedoraplanet.org/atom.xml":"planetFedoraReport.txt"}
 
-	title = i['title'].encode('utf-8')
-	report.write("Title :      "+str(title)+"\n")
+for source in sources:
+	feed = feedparser.parse(source)
+	report = open(files[source], "wb")
+	i=0
+	for entry in feed.entries:
+		i += 1
+		entry = dict(entry)
+		try:
+			title = entry['title'].encode('utf-8')
+		except Exception, e:
+			print "Exception :"+ str(e)
+			title = None
+		try:
+			updated = entry['updated'].encode('utf-8')
+		except Exception, e:
+			print e
+			updated = entry['published'].encode('utf-8')
+		report.write(str(i)+". Title :      "+str(title)+"\t on "+str(updated)+"\n")
 
-	updated = i['updated'].encode('utf-8')
-	report.write("On :"+ str(updated)+"\n")
+		# try:
+		# 	updated = entry['updated'].encode('utf-8')
+		# except Exception, e:
+		# 	print "Exception :"+ str(e)
+		# 	updated = None
+		# report.write("On :"+ str(updated)+"\n")
 
-	planetName = d['feed']['generator'].encode('utf-8')
-	report.write("Planet Name :" +str(planetName)+"\n")
+		try:
+			planetName = feed['feed']['title'].encode('utf-8')
+		except Exception, e:
+			print "Exception :"+ str(e)
+			planetName = None
+		report.write("  Planet Name :" +str(planetName)+"\n")
 
-	blogTitle = i['title'].encode('utf-8')
-	report.write("Blog Title : "+ str(blogTitle)+"\n")
+		try:
+			blogTitle = entry['title'].encode('utf-8')
+		except Exception, e:
+			print "Exception :"+ str(e)
+			blogTitle = None
+		report.write("  Blog Title : "+ str(blogTitle)+"\n")
 
-	author=i['author'].encode('utf-8')
-	report.write("Author : "+ str(author)+"\n")
+		try:
+			author = entry['author'].encode('utf-8')
+		except Exception, e:
+			print "Exception :"+ str(e)
+			author = None
+		report.write("  Author : "+ str(author)+"\n")
 
-	content = i['summary'].encode('utf-8')
-	report.write("Contents : "+str(content)+"\n")
+		try:
+			content = entry['summary'].encode('utf-8')
+		except Exception, e:
+			print "Exception :"+ str(e)
+			content = None
+		report.write("  Contents : "+str(content)+"\n")
 
-	for l in i['links']:
-		link = l['href'].encode('utf-8')
-		report.write("links : "+ str(link)+"\n")
-	for k in range(1, 2) :
-		report.write('\n')
+		for l in entry['links']:
+			try:
+				link = l['href'].encode('utf-8')
+			except Exception, e:
+				print "Exception :"+ str(e)
+				link = None
+			report.write("  links : "+ str(link)+"\n")
+		report.write("\n \n")
+	print 'done with '+ str(files[source])
 report.close()
-entries.close()
